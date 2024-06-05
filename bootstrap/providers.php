f17ba9103b17de0a382991e6dfaff2d3302b3b68
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2024-06-05 10:12:14 +0300

    Removed telescope provider.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\WeatherApiServiceProvider::class,
     Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 ];
