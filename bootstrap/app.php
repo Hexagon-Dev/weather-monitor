b5fcd39f821b23e40b5f770726808dfcf9a80a1a
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2024-06-08 15:01:48 +0300

    Session fix. #2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
-		$middleware->web([\Illuminate\Session\Middleware\StartSession::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- resources/js/plugins/api.ts
+++ resources/js/plugins/api.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: '/api/',
   headers: { 'Content-Type': 'application/json' },
+	withCredentials: true,
+	withXSRFToken: true,
 });
 
 api.interceptors.response.use(
